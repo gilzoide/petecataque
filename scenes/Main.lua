bee2564894811b24fd768c64240c2be403db177a
gilzoide <gilzoide@gmail.com>
2020-06-08 18:02:51 -0300

    Test more than one entity

--- scenes/Main.lua
+++ scenes/Main.lua
@@ -1,1 +1,6 @@
-return { { "Teste" } }
+return { { "Teste" }, { "Teste",
+    color = { 0.5, 0.5, 0.5 },
+    rectangle = { 100, 100, 100, 100 }
+  }, { "Teste",
+    rectangle = { 300, 100, 100, 100 }
+  } }
