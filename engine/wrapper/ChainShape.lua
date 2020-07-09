9728fcdbc3ec9c3673bbf4107e82955707d5738a
gilzoide <gilzoide@gmail.com>
2020-07-09 14:55:24 -0300

    Add ChainShape

---
+++ engine/wrapper/ChainShape.lua
@@ -0,0 +1,28 @@
+local wrapper = require 'wrapper'
+
+local ChainShape = wrapper.new('ChainShape', {
+    'getChildEdge', 'getNextVertex', 'getPoints', 'getPreviousVertex', 'getVertexCount',
+    'getChildCount', 'getRadius', 'getType',
+}, {
+    'setNextVertex', 'setPreviousVertex',
+}, {
+    'getPoint', 'computeAABB', 'computeMass', 'rayCast', 'testPoint',
+})
+
+ChainShape.points = { 0, 0, 0, 1 }
+ChainShape.loop = true
+
+function ChainShape:create_wrapped()
+    local shape = love.physics.newChainShape(self.loop, self.points)
+    return shape
+end
+
+function ChainShape:draw()
+    if self.drawmode == 'line' then
+        love.graphics.line(self.points)
+    end
+end
+
+ChainShape["$shape"] = wrapper.get_wrapped
+
+return ChainShape
