f5d7584fdfdc389cd8737b9067941f596c58a865
gilzoide <gilzoide@gmail.com>
2020-07-12 21:34:17 -0300

    Add Fixture wrapper

---
+++ engine/wrapper/physics/Fixture.lua
@@ -0,0 +1,34 @@
+local Fixture = Recipe.wrapper.new('Fixture', {
+    'getBody', 'getCategory', 'getDensity', 'getFilterData',
+    'getFriction', 'getGroupIndex', 'getMask', 'getMassData',
+    'getRestitution', 'getShape', 'getUserData',
+    'isDestroyed', 'isSensor',
+}, {
+    'setCategory', 'setDensity', 'setFilterData', 'setFriction',
+    'setGroupIndex', 'setMask', 'setRestitution', 'setSensor',
+    'setUserData',
+}, {
+    'getBoundingBox', 'rayCast', 'testPoint',
+})
+
+function Fixture:create_wrapped()
+    local body, shape = self.body, self.shape
+    if not body then
+        body = self:first_parent_with('body')
+        self.body = body
+        log.warnassert(body, "Unable to create fixture without a body")
+    end
+    if not shape then
+        shape = self:first_parent_with('shape')
+        self.shape = shape
+        log.warnassert(shape, "Unable to create fixture without a shape")
+    end
+
+    if body and shape then
+        return love.physics.newFixture(body.body, shape.shape)
+    end
+end
+
+Fixture['$fixture'] = Recipe.wrapper.get_wrapped
+
+return Fixture
