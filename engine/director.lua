abf3aaf5dbda9dba895618d15322f7c6f9589ef8
gilzoide <gilzoide@gmail.com>
2020-07-16 12:56:22 -0300

    Fix hidden flag to skip children

--- engine/director.lua
+++ engine/director.lua
@@ -1,12 +1,12 @@
 local Director = {}
 
-local function iterate_scene()
+local function iterate_scene(skip_if_field)
     local iterator = nested.iterate(Scene)
     return function()
         local kp, obj, parent, skip
         repeat
             kp, obj, parent = iterator(skip)
-            skip = obj and obj.disabled
+            skip = obj and (obj.disabled or obj[skip_if_field])
         until not skip
         return kp, obj, parent
     end
@@ -24,21 +24,19 @@ end
 
 function Director.draw()
     local pop_list = { {-1} }
-    for kp, obj in iterate_scene() do
+    for kp, obj in iterate_scene('hidden') do
         while #kp <= pop_list[#pop_list][1] do
             local t = table.remove(pop_list)
             love.graphics.pop()
         end
-        if not obj.hidden then
-            if obj.draw then
-                if obj.draw_push then
-                    love.graphics.push(obj.draw_push)
-                    pop_list[#pop_list + 1] = { #kp, obj }
-                end
-                DEBUG.PUSH_CALL(obj, 'draw')
-                obj:draw()
-                DEBUG.POP_CALL(obj, 'draw')
+        if obj.draw then
+            if obj.draw_push then
+                love.graphics.push(obj.draw_push)
+                pop_list[#pop_list + 1] = { #kp, obj }
             end
+            DEBUG.PUSH_CALL(obj, 'draw')
+            obj:draw()
+            DEBUG.POP_CALL(obj, 'draw')
         end
     end
     for i = #pop_list, 2, -1 do
