2df4a8b927b4970c4f4a954370f493fc3c67d0f2
gilzoide <gilzoide@gmail.com>
2020-07-22 11:58:03 -0300

    Avoid tracking hidden files
    (caham, .DS_Store)

--- engine/resources/asset_map.lua
+++ engine/resources/asset_map.lua
@@ -13,25 +13,27 @@ end
 
 local function scan_dir(self, dir, info)
     for i, item in ipairs(love.filesystem.getDirectoryItems(dir)) do
-        local path = dir .. '/' .. item
-        love.filesystem.getInfo(path, info)
-        if info.type == 'directory' then
-            scan_dir(self, path, info)
-        elseif info.type == 'file' then
-            self[path] = path
-            if self[item] == nil then
-                self[item] = path
-            elseif self[item] ~= false then
-                log.warnassert(false, "Resource filename at path %q matches %q. Disabling loading filename %q", path, self[item], item)
-                self[item] = false
-            end
+        if not item:startswith('.') then
+            local path = dir .. '/' .. item
+            love.filesystem.getInfo(path, info)
+            if info.type == 'directory' then
+                scan_dir(self, path, info)
+            elseif info.type == 'file' then
+                self[path] = path
+                if self[item] == nil then
+                    self[item] = path
+                elseif self[item] ~= false then
+                    log.warnassert(false, "Resource filename at path %q matches %q. Disabling loading filename %q", path, self[item], item)
+                    self[item] = false
+                end
 
-            local basename, ext = AssetMap.split_extension(item)
-            if self[basename] == nil then
-                self[basename] = path
-            elseif self[basename] ~= false then
-                log.warnassert(false, "Resource basename at path %q matches %q. Disabling loading basename %q", path, self[basename], basename)
-                self[basename] = false
+                local basename, ext = AssetMap.split_extension(item)
+                if self[basename] == nil then
+                    self[basename] = path
+                elseif self[basename] ~= false then
+                    log.warnassert(false, "Resource basename at path %q matches %q. Disabling loading basename %q", path, self[basename], basename)
+                    self[basename] = false
+                end
             end
         end
     end
