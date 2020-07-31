226e8ee1900d7086604ef7c712e4d951f1bcb06e
gilzoide <gilzoide@gmail.com>
2020-07-30 21:42:23 -0300

    Amend fix AssetMap, change cached path for a table (WIP resource cache)

--- engine/resources/asset_map.lua
+++ engine/resources/asset_map.lua
@@ -11,6 +11,11 @@ function AssetMap.get_basename(path)
     return basename
 end
 
+local asset_map_mt = {
+    __pairs = default_object_pairs,
+    __mode = "v",
+}
+
 local function scan_dir(self, dir, info)
     for i, item in ipairs(love.filesystem.getDirectoryItems(dir)) do
         if not item:startswith('.') then
@@ -19,19 +24,20 @@ local function scan_dir(self, dir, info)
             if info.type == 'directory' then
                 scan_dir(self, path, info)
             elseif info.type == 'file' then
-                self[path] = path
+                local t = setmetatable({ __path = path }, asset_map_mt)
+                self[path] = t
                 if self[item] == nil then
-                    self[item] = path
+                    self[item] = t
                 elseif self[item] ~= false then
-                    log.warnassert(false, "Resource filename at path %q matches %q. Disabling loading filename %q", path, self[item], item)
+                    DEBUG.WARN("Resource filename at path %q matches %q. Disabling loading filename %q", path, self[item].path, item)
                     self[item] = false
                 end
 
                 local basename, ext = AssetMap.split_extension(item)
                 if self[basename] == nil then
-                    self[basename] = path
+                    self[basename] = t
                 elseif self[basename] ~= false then
-                    log.warnassert(false, "Resource basename at path %q matches %q. Disabling loading basename %q", path, self[basename], basename)
+                    DEBUG.WARN("Resource basename at path %q matches %q. Disabling loading basename %q", path, self[basename].path, basename)
                     self[basename] = false
                 end
             end
@@ -48,7 +54,9 @@ function AssetMap.new(search_paths)
 end
 
 function AssetMap:full_path(file)
-    return log.warnassert(self[file], "Couldn't find file %q", file)
+    local path = get(self, file, '__path')
+    DEBUG.WARNIF(not path, "Couldn't find file %q", file)
+    return path
 end
 
 return AssetMap
