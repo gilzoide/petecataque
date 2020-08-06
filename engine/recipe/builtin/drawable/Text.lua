56f0ba5df973eecb184feace4e161f24d6cb20a6
gilzoide <gilzoide@gmail.com>
2020-08-06 05:53:02 -0300

    Make Text extend Frame with both horizontal and vertical alignment

--- engine/recipe/builtin/drawable/Text.lua
+++ engine/recipe/builtin/drawable/Text.lua
@@ -1,9 +1,8 @@
-local drawable_common = require 'recipe.builtin.drawable._common'
-
 local Text = Recipe.wrapper.new('Text', {
+    extends = 'Frame',
     wrapped_index = 'drawable',
     getters = {
-        'getDimensions', 'getFont', 'getHeight', 'getWidth',
+        'getDimensions', 'getFont', -- 'getHeight', 'getWidth',
     },
     setters = {
         'setFont'
@@ -13,41 +12,64 @@ local Text = Recipe.wrapper.new('Text', {
     },
 })
 
-Text.font = love.graphics.getFont()
+local fallback_font = love.graphics.newFont()
 Text.align = 'left'
+Text.valign = 'top'
+
+local function update_text(self, text)
+    local width = self.width
+    if width then
+        self.drawable:setf(text, width, self.align)
+    else
+        self.drawable:set(text)
+    end
+end
 
 function Text:create_wrapped()
-    return love.graphics.newText(self.font, self.text)
+    return love.graphics.newText(self.font or fallback_font, self.text)
+end
+
+function Text:update(dt)
+    update_text(self, self.text)
 end
 
 Text.draw_push = 'transform'
-Text.draw = drawable_common.draw
-Text.hitTestFromOrigin = drawable_common.hitTestFromOrigin
-Object.add_setter(Text, 'anchorPoint', drawable_common.setAnchorPoint)
+function Text:draw()
+    love.graphics.translate(self.left, self.top)
+
+    local color = self.color
+    if color then
+        love.graphics.setColor(color)
+    end
+
+    local drawable = self.drawable
+    local drawable_height = drawable:getHeight()
+    if drawable_height > 0 then
+        local vspace, valign, y = self.height - drawable_height, self.valign
+        if valign == 'center' then
+            y = vspace * 0.5
+        elseif valign == 'bottom' then
+            y = vspace
+        else
+            y = 0
+        end
+        love.graphics.draw(self.drawable, 0, y)
+    end
+end
 
 Object.add_setter(Text, 'text', function(self, text)
-    local have_text = text and #text > 0
-    self:enable_method('draw', have_text)
-    if self.drawable then
-        if self.limit then
-            self.drawable:setf(text, self.limit, self.align)
-        else
-            self.drawable:set(text)
-        end
+    local is_getter = Expression.is_getter(text)
+    self:set_method_enabled('update', is_getter)
+    if not is_getter then
+        update_text(self, text)
     end
 end)
 
-Object.add_getter(Text, "left", function(self)
-    return self.x
+Object.add_getter(Text, 'width', function(self)
+    return self.drawable:getWidth()
 end)
-Object.add_getter(Text, "right", function(self)
-    return self.x + self.width
-end)
-Object.add_getter(Text, "top", function(self)
-    return self.y
-end)
-Object.add_getter(Text, "bottom", function(self)
-    return self.y + self.height
+Object.add_getter(Text, 'height', function(self)
+    return self.drawable:getHeight()
 end)
 
 return Text
