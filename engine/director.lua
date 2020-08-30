998ae83f6c6668627834a6ec1655ec8168f2f51c
gilzoide <gilzoide@gmail.com>
2020-08-29 23:07:23 -0300

    Cache Scene tree traversal into linearized tables for fast subsequent update/draw traversals

--- engine/director.lua
+++ engine/director.lua
@@ -1,52 +1,18 @@
-local table_stack = require 'table_stack'
-
 local Director = {}
-
-local function iterate_scene(scene, skip_if_field)
-    local iterator = scene:iterate()
-    return function()
-        local kp, obj, parent, skip
-        repeat
-            kp, obj, parent = iterator(skip)
-            skip = obj and obj[skip_if_field]
-        until not skip
-        return kp, obj, parent
-    end
-end
 
 function Director.update(dt, scene)
     scene = scene or Scene
     if scene.paused then return end
-    for kp, obj in iterate_scene(scene, 'paused') do
-        obj:invoke('update', dt)
-    end
-end
-
-
-local pop_list = table_stack.new()
-pop_list:push(-1)
-function pop_list:pop_until(depth)
-    while depth <= self:peek()[1] do
-        local method, obj = unpack(self:pop(), 2)
-        method(obj)
+    for update, obj in scene:iterate_update() do
+        update(obj, dt)
     end
 end
 
 function Director.draw(scene)
     scene = scene or Scene
-    pop_list:clear(1)
-    for kp, obj in iterate_scene(scene, 'hidden') do
-        local depth = #kp
-        pop_list:pop_until(depth)
-        
-        obj:invoke('draw')
-
-        local late_draw = obj.late_draw
-        if late_draw then
-            pop_list:push(depth, late_draw, obj)
-        end
+    for draw, obj in scene:iterate_draw() do
+        draw(obj)
     end
-    pop_list:pop_until(0)
     DEBUG.ONLY(function()
         local stackDepth = love.graphics.getStackDepth()
         if stackDepth ~= 0 then
--- engine/scene.lua
+++ engine/scene.lua
@@ -1,53 +1,110 @@
+local table_pool = require 'table_pool'.raw
+local table_stack = require 'table_stack'
+
 local Scene = {}
 Scene.__index = Scene
 
 function Scene.new()
-    return setmetatable({}, Scene)
+    return setmetatable({
+        update = table_stack.new(),
+        draw = table_stack.new(),
+        dirty = true,
+    }, Scene)
 end
 
 function Scene:add(obj)
     self[#self + 1] = obj
-end
-
-function Scene:byId(id)
-    return self[id]
-end
-
-function Scene:byType(typename)
-    return self[typename]
-end
-
-function Scene:firstByType(typename)
-    return (get(self, typename, 1))
-end
-
-function Scene:track(obj)
-    local type = assert(obj[1], "Objects in scene must have a type")
-    set_or_create(self, type, obj, true)
-
-    if obj.id then
-        self[obj.id] = obj
-    end
-end
-
-function Scene:untrack(obj)
-    local type = assert(obj[1], "Objects in scene must have a type")
-    set(self, type, obj, nil)
-    
-    if obj.id then
-        self[obj.id] = nil
-    end
-end
-
-local iterator_flags = { skip_root = true, table_only = true }
-function Scene:iterate()
-    return nested.iterate(self, iterator_flags)
+    self.dirty = true
 end
 
 function Scene:release()
     for i, obj in ipairs(self) do
         Object.release(obj)
     end
+    self.dirty = true
+end
+
+-- Iteration and caching
+local iterator_flags = { skip_root = true, postorder = true, table_only = true }
+local function iterate_all_and_cache(scene, update_or_draw, skip_if_field)
+    scene.dirty = false
+    local update = scene.update; update:clear()
+    local draw = scene.draw; draw:clear()
+
+    local update_previous = table_pool:acquire()
+    local draw_previous = table_pool:acquire()
+    local skipping_object = false
+
+    local function process_obj_going_down(obj, method, stack, previous_cache, maybe_yield)
+        if method then
+            previous_cache[obj] = stack:push(method, obj, stack.n + 1)
+            if maybe_yield and not skipping_object then
+                if obj[skip_if_field] then
+                    skipping_object = obj
+                else
+                    coroutine.yield(method, obj)
+                end
+            end
+        end
+    end
+    local function process_obj_going_up(obj, method, stack, previous_cache, maybe_yield)
+        if method then
+            stack:push(method, obj, 0)
+            if maybe_yield and not skipping_object then
+                coroutine.yield(method, obj)
+            end
+        end
+        if previous_cache[obj] then
+            previous_cache[obj][3] = stack.n - previous_cache[obj][3]
+            previous_cache[obj] = nil
+        end
+        if skipping_object == obj then
+            skipping_object = false
+        end
+    end
+
+    local is_update = update_or_draw == 'update'
+    local is_draw = update_or_draw == 'draw'
+    for kp, obj, parent, going_down in nested.iterate(scene, iterator_flags) do
+        if going_down then
+            process_obj_going_down(obj, obj.update, update, update_previous, is_update)
+            process_obj_going_down(obj, obj.draw, draw, draw_previous, is_draw)
+        else
+            process_obj_going_up(obj, obj.late_update, update, update_previous, is_update)
+            process_obj_going_up(obj, obj.late_draw, draw, draw_previous, is_draw)
+        end
+    end
+
+    table_pool:release(update_previous)
+    table_pool:release(draw_previous)
+end
+
+local function iterate_cached(scene, update_or_draw, skip_if_field)
+    local cache = scene[update_or_draw]
+    local i = 1
+    while i <= cache.n do
+        local method, obj, children = unpack(cache[i])
+        if not obj[skip_if_field] then
+            coroutine.yield(method, obj)
+            i = i + 1
+        else
+            i = i + 1 + children
+        end
+    end
+end
+
+function Scene:iterate_update()
+    local iterator_function = self.dirty and iterate_all_and_cache or iterate_cached
+    return coroutine.wrap(function() iterator_function(self, 'update', 'paused') end)
+end
+
+function Scene:iterate_draw()
+    local iterator_function = self.dirty and iterate_all_and_cache or iterate_cached
+    return coroutine.wrap(function() iterator_function(self, 'draw', 'hidden') end)
+end
+
+function Scene:iterate()
+    return nested.iterate(self, iterator_flags)
 end
 
 return Scene
