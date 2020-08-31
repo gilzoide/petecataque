69a6b63707d332106021aacbd621bde482a58f56
gilzoide <gilzoide@gmail.com>
2020-08-31 13:12:21 -0300

    Add DEBUG.P{USH,OP}_CALL to update and draw

--- engine/director.lua
+++ engine/director.lua
@@ -4,14 +4,18 @@ function Director.update(dt, scene)
     scene = scene or Scene
     if scene.paused then return end
     for update, obj in scene:iterate_update() do
+        DEBUG.PUSH_CALL(obj, 'update')
         update(obj, dt)
+        DEBUG.POP_CALL(obj, 'update')
     end
 end
 
 function Director.draw(scene)
     scene = scene or Scene
     for draw, obj in scene:iterate_draw() do
+        DEBUG.PUSH_CALL(obj, 'draw')
         draw(obj)
+        DEBUG.POP_CALL(obj, 'draw')
     end
     DEBUG.ONLY(function()
         local stackDepth = love.graphics.getStackDepth()
