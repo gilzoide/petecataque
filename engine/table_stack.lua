7b6ebe1fcf6967da3a78a9b084a1aa87cc94b75c
gilzoide <gilzoide@gmail.com>
2020-08-31 13:44:48 -0300

    Add tables in table_stack on __index

--- engine/table_stack.lua
+++ engine/table_stack.lua
@@ -1,6 +1,5 @@
 local table_stack = {}
 
-local weak_mt = { __mt = 'kv' }
 function table_stack.new()
     return setmetatable({ n = 0 }, table_stack)
 end
@@ -8,10 +7,6 @@ end
 function table_stack:push(...)
     self.n = self.n + 1
     local entry = self[self.n]
-    if not entry then
-        entry = setmetatable({}, weak_mt)
-        self[self.n] = entry
-    end
     for i = 1, select('#', ...) do
         entry[i] = select(i, ...)
     end
@@ -32,11 +27,18 @@ function table_stack:clear(n)
     self.n = n or 0
 end
 
-table_stack.__index = {
+local methods = {
     push = table_stack.push,
     peek = table_stack.peek,
     pop = table_stack.pop,
     clear = table_stack.clear,
 }
+local weak_mt = { __mt = 'kv' }
+function table_stack:__index(index)
+    if methods[index] then return methods[index] end
+    local new_table = setmetatable({}, weak_mt)
+    rawset(self, index, new_table)
+    return new_table
+end
 
 return table_stack
